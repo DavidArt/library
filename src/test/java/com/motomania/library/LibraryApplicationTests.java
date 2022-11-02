3d3a34ad93424a7ce8bc677304810f42726efbff
David <davidrus.art@gmail.com>
2022-11-02 13:05:17 +0200

    Initial commit

---
+++ src/test/java/com/motomania/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motomania.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
